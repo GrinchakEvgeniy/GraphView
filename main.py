09bdc22c3414326514f5e25cf977ad7dd5455dfa
GrinchakEvgeniy <kukulalatutu@gmail.com>
2019-08-27 20:53:50 +0300

    second commit

--- main.py
+++ main.py
@@ -4,22 +4,19 @@ from kivy.clock import Clock
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 from kivy.garden.graph import Graph, MeshLinePlot
-from data_cord import *
+from data_cord import first_data, second_data
 from math import *
 import parser
+
 
 class Graphics(BoxLayout):
 	def __init__(self,):
 		super(Graphics, self).__init__()
 		self.plot = MeshLinePlot(color=[1,0,0,1])
-
-		self.ids.graph.add_plot(self.plot)
-		# Clock.schedule_interval(self.get_formul, 0.001)
-		# self.plot.points = self.get_value()
+		
 
 	def parser_formul(self,string_formula,x):
 		return eval(parser.expr(string_formula).compile())
-		
 
 	def get_value(self, formula=""):
 		n = 0
@@ -29,15 +26,20 @@ class Graphics(BoxLayout):
 				n += 1
 			return first_data
 		return first_data
-		
+
+	# def start(self):
+	# 	self.ids.graph.add_plot(self.plot)
+	# 	Clock.schedule_interval(self.get_formul, 0.1)
 
 	def get_formul(self):
 		formul = self.text_input.text
+		self.ids.graph.add_plot(self.plot)
 		self.plot.points = self.get_value(formul)
+		formul = ''
 
 class MyApp(App):
 	def build(self):
 		return Graphics()
 		
-
-MyApp().run()
+if __name__ == "__main__":
+	MyApp().run()
